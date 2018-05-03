f9e506561ebf29e30ea50f545c9d1d0840d31e6e
Jan Moxter <jan.moxter@innobix.com>
2018-05-03 15:21:26 -0500

    - adding controller for admin

---
+++ src/main/java/org/efaps/pos/controller/AdminController.java
@@ -0,0 +1,29 @@
+package org.efaps.pos.controller;
+
+import org.efaps.pos.service.SyncService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("admin")
+public class AdminController
+{
+
+    /** The sync service. */
+    private final SyncService syncService;
+
+    @Autowired
+    public AdminController(final SyncService _syncService)
+    {
+        this.syncService = _syncService;
+    }
+
+    @GetMapping(path = "/sync")
+    public void sync()
+    {
+        this.syncService.syncProducts();
+        this.syncService.syncCategories();
+    }
+}
