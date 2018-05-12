9d75d628dc277c180a33ec75ddd35cbaf6cdca77
Jan Moxter <jan.moxter@innobix.com>
2018-05-12 15:19:44 -0500

    - adding some error handling

--- src/main/java/org/efaps/pos/client/EFapsClient.java
+++ src/main/java/org/efaps/pos/client/EFapsClient.java
@@ -29,12 +29,15 @@ import org.efaps.pos.dto.ReceiptDto;
 import org.efaps.pos.dto.UserDto;
 import org.efaps.pos.dto.WorkspaceDto;
 import org.efaps.pos.sso.SSOClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.MediaType;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -43,6 +46,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 public class EFapsClient
 {
 
+    private static final Logger LOG = LoggerFactory.getLogger(EFapsClient.class);
     private final ConfigProperties config;
     private final RestTemplate restTemplate;
     private final SSOClient ssoClient;
@@ -59,74 +63,107 @@ public class EFapsClient
 
     public List<ProductDto> getProducts()
     {
-        final RequestEntity<?> requestEntity = get(this.config.getEFaps().getProductPath());
-        final ResponseEntity<List<ProductDto>> ret = this.restTemplate.exchange(requestEntity,
-                        new ParameterizedTypeReference<List<ProductDto>>()
-                        {
-                        });
-        return ret.getBody();
+        List<ProductDto> ret = null;
+        try {
+            final RequestEntity<?> requestEntity = get(this.config.getEFaps().getProductPath());
+            final ResponseEntity<List<ProductDto>> response = this.restTemplate.exchange(requestEntity,
+                            new ParameterizedTypeReference<List<ProductDto>>()
+                            {
+                            });
+            ret = response.getBody();
+        } catch (final RestClientException e) {
+            LOG.error("Catched error during retrieval of products", e);
+        }
+        return ret;
     }
 
     public List<CategoryDto> getCategories()
     {
-        final RequestEntity<?> requestEntity = get(this.config.getEFaps().getCategoryPath());
-        final ResponseEntity<List<CategoryDto>> ret = this.restTemplate.exchange(requestEntity,
-                        new ParameterizedTypeReference<List<CategoryDto>>()
-                        {
-                        });
-        return ret.getBody();
+        List<CategoryDto> ret = null;
+        try {
+            final RequestEntity<?> requestEntity = get(this.config.getEFaps().getCategoryPath());
+            final ResponseEntity<List<CategoryDto>> response = this.restTemplate.exchange(requestEntity,
+                            new ParameterizedTypeReference<List<CategoryDto>>()
+                            {
+                            });
+            ret = response.getBody();
+        } catch (final RestClientException e) {
+            LOG.error("Catched error during retrieval of categories", e);
+        }
+        return ret;
     }
 
     public List<WorkspaceDto> getWorkspaces()
     {
-        final RequestEntity<?> requestEntity = get(this.config.getEFaps().getWorkspacePath());
-        final ResponseEntity<List<WorkspaceDto>> ret = this.restTemplate.exchange(requestEntity,
-                        new ParameterizedTypeReference<List<WorkspaceDto>>()
-                        {
-                        });
-        return ret.getBody();
+        List<WorkspaceDto> ret = null;
+        try {
+            final RequestEntity<?> requestEntity = get(this.config.getEFaps().getWorkspacePath());
+            final ResponseEntity<List<WorkspaceDto>> response = this.restTemplate.exchange(requestEntity,
+                            new ParameterizedTypeReference<List<WorkspaceDto>>()
+                            {
+                            });
+            ret = response.getBody();
+        } catch (final RestClientException e) {
+            LOG.error("Catched error during retrieval of workspaces", e);
+        }
+        return ret;
     }
 
     public List<PosDto> getPOSs()
     {
-        final RequestEntity<?> requestEntity = get(this.config.getEFaps().getPosPath());
-        final ResponseEntity<List<PosDto>> ret = this.restTemplate.exchange(requestEntity,
-                        new ParameterizedTypeReference<List<PosDto>>()
-                        {
-                        });
-        return ret.getBody();
+        List<PosDto> ret = null;
+        try {
+            final RequestEntity<?> requestEntity = get(this.config.getEFaps().getPosPath());
+            final ResponseEntity<List<PosDto>> response = this.restTemplate.exchange(requestEntity,
+                            new ParameterizedTypeReference<List<PosDto>>()
+                            {
+                            });
+            ret = response.getBody();
+        } catch (final RestClientException e) {
+            LOG.error("Catched error during retrieval of poss", e);
+        }
+        return ret;
     }
 
     public List<UserDto> getUsers()
     {
-        final RequestEntity<?> requestEntity = get(this.config.getEFaps().getUserPath());
-        final ResponseEntity<List<UserDto>> ret = this.restTemplate.exchange(requestEntity,
-                        new ParameterizedTypeReference<List<UserDto>>()
-                        {
-                        });
-        return ret.getBody();
+        List<UserDto> ret = null;
+        try {
+            final RequestEntity<?> requestEntity = get(this.config.getEFaps().getUserPath());
+            final ResponseEntity<List<UserDto>> response = this.restTemplate.exchange(requestEntity,
+                            new ParameterizedTypeReference<List<UserDto>>()
+                            {
+                            });
+            ret = response.getBody();
+        } catch (final RestClientException e) {
+            LOG.error("Catched error during retrieval of users", e);
+        }
+        return ret;
     }
 
-    public ReceiptDto postReceipt(final ReceiptDto _receipt) {
+    public ReceiptDto postReceipt(final ReceiptDto _receipt)
+    {
         final RequestEntity<ReceiptDto> requestEntity = post(this.config.getEFaps().getReceiptPath(), _receipt);
 
         final ResponseEntity<ReceiptDto> ret = this.restTemplate.exchange(requestEntity, ReceiptDto.class);
         return ret.getBody();
     }
 
-    public Checkout checkout(final String _oid) {
-        final URI uri = UriComponentsBuilder
-            .fromUri(this.config.getEFaps().getBaseUrl())
-            .path(this.config.getEFaps().getCheckoutPath())
-            .queryParam("oid", _oid)
-            .build()
-            .toUri();
-        final RequestEntity<?> requestEntity = get(uri);
-        final ResponseEntity<byte[]> response = this.restTemplate.exchange(requestEntity, byte[].class);
-        final Checkout ret = new Checkout();
-        ret.setContent(response.getBody());
-        ret.setContentType(response.getHeaders().getContentType());
-        ret.setFilename(response.getHeaders().getContentDisposition().getFilename());
+    public Checkout checkout(final String _oid)
+    {
+        Checkout ret = null;
+        try {
+            final URI uri = UriComponentsBuilder.fromUri(this.config.getEFaps().getBaseUrl()).path(this.config
+                            .getEFaps().getCheckoutPath()).queryParam("oid", _oid).build().toUri();
+            final RequestEntity<?> requestEntity = get(uri);
+            final ResponseEntity<byte[]> response = this.restTemplate.exchange(requestEntity, byte[].class);
+            ret = new Checkout();
+            ret.setContent(response.getBody());
+            ret.setContentType(response.getHeaders().getContentType());
+            ret.setFilename(response.getHeaders().getContentDisposition().getFilename());
+        } catch (final RestClientException e) {
+            LOG.error("Catched error during checkout for oid: '{}'", _oid);
+        }
         return ret;
     }
 
@@ -136,26 +173,20 @@ public class EFapsClient
         if (this.config.getSso() != null && this.config.getSso().getUrl() != null) {
             auth = "Bearer " + this.ssoClient.getToken();
         } else if (this.config.getAuth() != null) {
-            auth = "Basic " + Base64.getEncoder()
-                .encodeToString((this.config.getAuth().getUser() + ":" + this.config.getAuth().getPassword())
-                                .getBytes(StandardCharsets.UTF_8));
+            auth = "Basic " + Base64.getEncoder().encodeToString((this.config.getAuth().getUser() + ":" + this.config
+                            .getAuth().getPassword()).getBytes(StandardCharsets.UTF_8));
         }
         return auth;
     }
 
     private UriComponents getUriComponent(final String _path)
     {
-        return UriComponentsBuilder
-                        .fromUri(this.config.getEFaps().getBaseUrl())
-                        .path(_path)
-                        .build();
+        return UriComponentsBuilder.fromUri(this.config.getEFaps().getBaseUrl()).path(_path).build();
     }
 
     private RequestEntity<?> get(final URI _uri)
     {
-        return RequestEntity.get(_uri)
-                        .header("Authorization", getAuth())
-                        .build();
+        return RequestEntity.get(_uri).header("Authorization", getAuth()).build();
     }
 
     private RequestEntity<?> get(final String _path)
@@ -165,9 +196,7 @@ public class EFapsClient
 
     private <T> RequestEntity<T> post(final String _path, final T _body)
     {
-        return RequestEntity.post(getUriComponent(_path).toUri())
-                        .header("Authorization", getAuth())
-                        .accept(MediaType.APPLICATION_JSON)
-                        .body(_body);
+        return RequestEntity.post(getUriComponent(_path).toUri()).header("Authorization", getAuth()).accept(
+                        MediaType.APPLICATION_JSON).body(_body);
     }
 }
