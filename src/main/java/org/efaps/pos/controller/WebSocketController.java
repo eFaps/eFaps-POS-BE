f89f4345cbc8f04700bc50df0793841556c7eea3
Jan Moxter <jan.moxter@innobix.com>
2018-05-31 19:17:17 -0500

    Control of currently edited orders

--- src/main/java/org/efaps/pos/controller/WebSocketController.java
+++ src/main/java/org/efaps/pos/controller/WebSocketController.java
@@ -17,36 +17,53 @@
 
 package org.efaps.pos.controller;
 
+import java.util.Collection;
+
+import org.efaps.pos.service.WebSocketService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.annotation.SubscribeMapping;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class WebSocketController
 {
+    private static final Logger LOG = LoggerFactory.getLogger(WebSocketController.class);
 
     private final SimpMessagingTemplate messagingTemplate;
+    private final WebSocketService webSocketService;
 
-    public WebSocketController(final SimpMessagingTemplate _messagingTemplate) {
+    public WebSocketController(final SimpMessagingTemplate _messagingTemplate,
+                               final WebSocketService _webSocketService) {
         this.messagingTemplate = _messagingTemplate;
+        this.webSocketService = _webSocketService;
+    }
+
+    @SubscribeMapping("/orders/start.edit")
+    public Collection<String> subscribeOrderStartEdit() {
+        return this.webSocketService.getOrdersEdited();
     }
 
     @MessageMapping("/orders/start.edit")
     public String processOrderStartEdit(@Payload final String _message)
         throws Exception
     {
-        System.out.println(_message);
-        return "Hallo";
+        LOG.debug("Recieved edit start for : {}", _message);
+        this.webSocketService.addOrder(_message);
+        return _message;
     }
 
     @MessageMapping("/orders/finish.edit")
     public String processOrderFinishEdit(@Payload final String _message)
         throws Exception
     {
-        System.out.println(_message);
-        return "Finish";
+        LOG.debug("Recieved edit finish for : {}", _message);
+        this.webSocketService.removeOrder(_message);
+        return _message;
     }
 
     @MessageExceptionHandler
