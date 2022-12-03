daa5a3add6de048bbe8ef40f9e2b567964592fec
Jan Moxter <jan@moxter.net>
2022-12-02 19:24:55 -0500

    Set version

--- src/test/java/org/efaps/pos/service/BalanceServiceTest.java
+++ src/test/java/org/efaps/pos/service/BalanceServiceTest.java
@@ -30,6 +30,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureMockRestServiceServer;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
