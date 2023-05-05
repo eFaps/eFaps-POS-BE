e578793d285c4a88d1e6553d99cee506d42117cd
Jan Moxter <jan@moxter.net>
2023-05-04 22:47:07 -0500

    Different test

--- src/test/java/org/efaps/pos/service/JobServiceTest.java
+++ src/test/java/org/efaps/pos/service/JobServiceTest.java
@@ -39,9 +39,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.boot.test.autoconfigure.data.mongo.AutoConfigureDataMongo;
 import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureMockRestServiceServer;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.test.context.ActiveProfiles;
@@ -51,7 +52,8 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @AutoConfigureMockMvc
 @AutoConfigureMockRestServiceServer
 @ActiveProfiles(profiles = "test")
-@DataMongoTest
+@AutoConfigureDataMongo
+@SpringBootTest
 public class JobServiceTest
 {
 
