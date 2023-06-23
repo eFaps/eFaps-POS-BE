567b2b90464a1bce3f3f55a1c2afc81d0cbbf2bb
Jan Moxter <jan@moxter.net>
2023-06-23 14:38:16 -0500

    Proxy shit

--- src/main/java/org/efaps/pos/config/TemplateCustomizer.java
+++ src/main/java/org/efaps/pos/config/TemplateCustomizer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003 - 2019 The eFaps Team
+ * Copyright 2003 - 2023 The eFaps Team
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,17 +16,41 @@
  */
 package org.efaps.pos.config;
 
+import java.net.URISyntaxException;
+import java.util.Optional;
+
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
+import org.apache.hc.client5.http.impl.routing.DefaultProxyRoutePlanner;
+import org.apache.hc.core5.http.HttpHost;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.client.BufferingClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 public class TemplateCustomizer
     implements org.springframework.boot.web.client.RestTemplateCustomizer
 {
 
+    @Value("${proxyUri:#{null}}")
+    private Optional<String> proxyUri;
+
     @Override
     public void customize(final RestTemplate _restTemplate)
     {
-        _restTemplate.setRequestFactory(new BufferingClientHttpRequestFactory(_restTemplate.getRequestFactory()));
+        if (proxyUri.isPresent()) {
+            try {
+                final HttpHost proxy = HttpHost.create(proxyUri.get());
+                final CloseableHttpClient httpClient = HttpClientBuilder.create()
+                                .setRoutePlanner(new DefaultProxyRoutePlanner(proxy)).build();
+                _restTemplate.setRequestFactory(new BufferingClientHttpRequestFactory(
+                                new HttpComponentsClientHttpRequestFactory(httpClient)));
+            } catch (final URISyntaxException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            _restTemplate.setRequestFactory(new BufferingClientHttpRequestFactory(_restTemplate.getRequestFactory()));
+        }
         _restTemplate.getInterceptors().add(new RequestResponseLoggingInterceptor());
     }
 }
