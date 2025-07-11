bedab2adca9cea6ab850d0a77cee46944aaebc6e
Jan Moxter <jan.moxter@moxter.net>
2025-07-10 20:33:25 -0500

    Correct test

--- src/test/java/org/efaps/pos/service/UpdateServiceTest.java
+++ src/test/java/org/efaps/pos/service/UpdateServiceTest.java
@@ -18,6 +18,7 @@ package org.efaps.pos.service;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.Arrays;
 
 import org.apache.commons.compress.archivers.ArchiveException;
@@ -25,6 +26,7 @@ import org.apache.commons.io.IOUtils;
 import org.efaps.pos.client.EFapsClient;
 import org.efaps.pos.dto.UpdateDto;
 import org.efaps.pos.dto.UpdateInstructionDto;
+import org.efaps.pos.entity.Identifier;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -32,6 +34,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.AutoConfigureDataMongo;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.http.ContentDisposition;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -49,16 +52,24 @@ public class UpdateServiceTest
 {
 
     @Autowired
+    private MongoTemplate mongoTemplate;
+
+    @Autowired
     private UpdateService updateService;
     @Autowired
     private EFapsClient eFapsClient;
 
     private MockRestServiceServer server;
 
+
     @BeforeEach
     void init()
     {
         server = MockRestServiceServer.createServer(eFapsClient.getRestTemplate());
+        mongoTemplate.save(new Identifier()
+                        .setId(Identifier.KEY)
+                        .setCreated(LocalDateTime.now())
+                        .setIdentifier("FAKEIDENT"));
     }
 
     @Test
@@ -70,21 +81,21 @@ public class UpdateServiceTest
         final var orange = IOUtils.toByteArray(getClass().getClassLoader().getResourceAsStream("orange.jpeg"));
         final var spa = IOUtils.toByteArray(getClass().getClassLoader().getResourceAsStream("spa.zip"));
 
-        server.expect(requestTo("http://localhost:8888/eFaps/api/checkout?oid=1.1"))
+        server.expect(requestTo("http://localhost:8888/eFaps/api/checkout?oid=1.1&ident=FAKEIDENT"))
                         .andRespond(MockRestResponseCreators
                                         .withSuccess(apple, MediaType.IMAGE_JPEG)
                                         .header(HttpHeaders.CONTENT_DISPOSITION,
                                                         ContentDisposition.attachment().filename("apple.jpeg").build()
                                                                         .toString()));
 
-        server.expect(requestTo("http://localhost:8888/eFaps/api/checkout?oid=1.2"))
+        server.expect(requestTo("http://localhost:8888/eFaps/api/checkout?oid=1.2&ident=FAKEIDENT"))
                         .andRespond(MockRestResponseCreators
                                         .withSuccess(orange, MediaType.IMAGE_JPEG)
                                         .header(HttpHeaders.CONTENT_DISPOSITION,
                                                         ContentDisposition.attachment().filename("orange.jpeg").build()
                                                                         .toString()));
 
-        server.expect(requestTo("http://localhost:8888/eFaps/api/checkout?oid=1.3"))
+        server.expect(requestTo("http://localhost:8888/eFaps/api/checkout?oid=1.3&ident=FAKEIDENT"))
         .andRespond(MockRestResponseCreators
                         .withSuccess(spa, MediaType.valueOf("application/zip"))
                         .header(HttpHeaders.CONTENT_DISPOSITION,
